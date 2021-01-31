2d75e584b4c7b90e771711a7c58e7946f3d633e1
Maxime Bret <maxime.bret@rakuten.com>
2021-01-31 14:56:57 +0900

    fixed file storage on ios iphone

--- src/storageAssets.ts
+++ src/storageAssets.ts
@@ -1,9 +1,12 @@
 import { useEffect, useMemo, useState } from "react"
 import { atom, selectorFamily, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil"
 import localforage from 'localforage'
-import { useMountedState } from "react-use"
 
 const MAX_RETRY_TIME = 2
+const RETRY_RETRIEVING_ASSET_AFTER = 5000
+
+type Base64 = string
+type FileType = Blob | Base64
 
 type Asset = {
   id: string,
@@ -11,7 +14,7 @@ type Asset = {
   cacheBuster: number,
   resources: string[]
   downloading: boolean
-  state: undefined | 'missing' | 'downloaded',
+  state: undefined | 'missing' | 'downloaded' | 'failed',
   missingHigherOrder: boolean
 }
 
@@ -23,6 +26,21 @@ const assetsState = atom<Asset[]>({
 const assetState = selectorFamily({
   key: 'assetState',
   get: (id: string) => ({ get }) => get(assetsState).find(asset => asset.id === id)
+})
+
+const fileToBase64 = (file: Blob) => new Promise((resolve, reject) => {
+  try {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.addEventListener("load", () => {
+      resolve(reader.result);
+    });
+    reader.addEventListener("error", error => {
+      reject(error);
+    });
+  } catch (e) {
+    reject(e)
+  }
 })
 
 const retryFetch = async (resource: string, retryNumber = 0): Promise<Blob> => {
@@ -42,36 +60,76 @@ const retryFetch = async (resource: string, retryNumber = 0): Promise<Blob> => {
   }
 }
 
-const useStorageAsset = (id: string) => {
+const useStorageAssetFromStorage = (id: string) => {
   const { cacheBuster, id: existingId, state } = useRecoilValue(assetState(id)) || {}
   const [data, setData] = useState<Blob | undefined>()
-  const isMounted = useMountedState()
   const setAssetsState = useSetRecoilState(assetsState)
 
+  /**
+   * When download/storage fails for some reason we will wait a littbe bit
+   * and reset the state so that we restart the whole process
+   */
   useEffect(() => {
-    if (!existingId || state === 'missing') return
+    let timer: ReturnType<typeof setTimeout>
+
+    if (state === 'failed') {
+      timer = setTimeout(() => {
+        setAssetsState(old => {
+          const item = old.find((old => old.id === existingId))
+
+          if (!item || item.state !== 'failed') return old
+
+          return old.map(item => item.id === existingId ? { ...item, state: undefined, } : item)
+        })
+      }, RETRY_RETRIEVING_ASSET_AFTER)
+    }
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [state, setAssetsState, existingId])
+
+  useEffect(() => {
+    let cancelled = false
+
+    if (!existingId || state === 'missing' || state === 'failed') return
 
       ; (async () => {
-        let data = await localforage.getItem<({ blob: Blob, order: number }) | null>(existingId)
+        let data = await localforage.getItem<({ blob: FileType, order: number }) | null>(existingId)
 
-        if (data && isMounted()) {
-          setData(data.blob)
-          if (state !== 'downloaded') {
-            setAssetsState(old => old.map(item => item.id === existingId
-              ? {
-                ...item,
-                state: 'downloaded',
-                missingHigherOrder: (data?.order || 0) < (item.resources.length - 1),
-              }
-              : item
-            ))
+        if (data && !cancelled) {
+          let blobData: Blob | undefined
+          if (typeof data.blob === 'string') {
+            const response = await fetch(data.blob)
+            blobData = await response.blob()
+          } else {
+            blobData = data.blob
+          }
+
+          if (!cancelled) {
+            setData(blobData)
+            if (state !== 'downloaded') {
+              setAssetsState(old => old.map(item => item.id === existingId
+                ? {
+                  ...item,
+                  state: 'downloaded',
+                  missingHigherOrder: (data?.order || 0) < (item.resources.length - 1),
+                }
+                : item
+              ))
+            }
           }
         }
-        if (!data && isMounted()) {
+        
+        if (!data && !cancelled) {
           setAssetsState(old => old.map(item => item.id === existingId ? { ...item, state: 'missing' } : item))
         }
       })()
-  }, [cacheBuster, isMounted, existingId, state, setAssetsState])
+
+      return () => {
+        cancelled = true
+      }
+  }, [cacheBuster, existingId, state, setAssetsState])
 
   useEffect(() => {
     setData(undefined)
@@ -83,7 +141,7 @@ const useStorageAsset = (id: string) => {
 export const useAsset = (id: string, resources: string[]) => {
   const { id: existingAssetId } = useRecoilValue(assetState(id)) || {}
   const setAssetsState = useSetRecoilState(assetsState)
-  const data = useStorageAsset(id)
+  const data = useStorageAssetFromStorage(id)
 
   useEffect(() => {
     setAssetsState(old => {
@@ -96,7 +154,6 @@ export const useAsset = (id: string, resources: string[]) => {
 
   return useMemo(() => ({
     asset: data, retry: () => {
-
       console.log('retry')
     }
   }), [data])
@@ -106,8 +163,9 @@ export const StorageAssets = () => {
   const [assets, setAssets] = useRecoilState(assetsState)
 
   useEffect(() => {
-    const assetsToDownload = assets.filter(asset => (asset.state === 'missing' || asset.missingHigherOrder) && !asset.downloading)
+    const assetsToDownload = assets.filter(asset => (asset.state === 'missing' || asset.missingHigherOrder) && !asset.downloading && !(asset.state === 'failed'))
 
+    console.log(assetsToDownload)
     if (assetsToDownload.length === 0) return
 
     setAssets(old => old.map(old => {
@@ -134,14 +192,24 @@ export const StorageAssets = () => {
                   if (indexOrder < startFromIndexOrder) return
                   const blob = await retryFetch(resource)
                   const data = { blob: blob, order: indexOrder }
-                  const existingData = await localforage.getItem<{ blob: Blob, order: number }>(id)
+                  const existingData = await localforage.getItem<{ blob: FileType, order: number }>(id)
                   if (data.order >= (existingData?.order || 0)) {
-                    await localforage.setItem(id, data)
+                    try {
+                      await localforage.setItem(id, data)
+                    } catch (e) {
+                      // for some browser or safari, blob is not always supported so we try to store as b64
+                      if (e.name === 'DataCloneError' && e.code === 25) {
+                        const base64CompatibleData = { blob: await fileToBase64(blob), order: indexOrder }
+                        await localforage.setItem(id, base64CompatibleData)
+                      } else {
+                        throw e
+                      }
+                    }
                     setAssets(old => old.map(item => item.id === id ? { ...item, state: 'downloaded', downloading: false, cacheBuster: item.cacheBuster + 1 } : item))
                   }
                 }))
               } catch (e) {
-                setAssets(old => old.map(item => item.id === id ? { ...item, downloading: false } : item))
+                setAssets(old => old.map(item => item.id === id ? { ...item, downloading: false, state: 'failed' } : item))
                 console.error(e)
               }
             })()
